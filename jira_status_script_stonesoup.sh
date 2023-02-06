2a9da7f532840f17d30cf4e8dc8bcfcf251448f8
Abhi <abhinandan13jan@gmail.com>
2023-02-06 18:58:45 +0530

    tr1

--- jira_status_script_stonesoup.sh
+++ jira_status_script_stonesoup.sh
@@ -31,7 +31,7 @@ if [ $alert_check -ge 45 ]; then
 fi
 hac_bugs+="\n4. <https://issues.redhat.com/issues/?filter=12396778 | Triaged Bugs> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396778' -H "Accept: application/json" | jq '.total'))" 
 
-# this is block 3
+# this is third block
 echo "Fetching stonesoup dashboard"
 
 stonesoup_dashboard="\n\n *StoneSoup Dashboard:* :clipboard::clipboard:"
@@ -44,7 +44,7 @@ fi
 stonesoup_dashboard+="\n3. <https://issues.redhat.com/issues/?filter=12405358 | Stonesoup UI - Needs UX> ($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12405358' -H "Accept: application/json" | jq '.total'))" 
 alert_check=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12405686' -H "Accept: application/json" | jq '.total')
 stonesoup_dashboard+="\n4. <https://issues.redhat.com/issues/?filter=12405686 | Untriaged Stonesoup bugs> ($alert_check)" 
-if [ $alert_check -ge 45 ]; then
+if [ $alert_check -ge 48 ]; then
     stonesoup_dashboard+=" :fire::fire:"
 fi
 
@@ -54,7 +54,7 @@ github_data="\n\n *GitHub filters:* :github: :github:"
 github_data+="\n<https://github.com/search?q=repo%3Aopenshift%2Fhac-dev+state%3Aopen&type=issues | HAC open PRs>:  $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fhac-dev+state%3Aopen&type=issues' -H "Accept: application/json" | jq '.total_count')"
 github_data+="\n<https://github.com/search?q=repo%3Aopenshift%2Fhac-dev+state%3Aopen+-label%3algtm&type=issues | HAC PRs without LGTM>:  $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fhac-dev+state%3Aopen+-label%3algtm&type=issues' -H "Accept: application/json" | jq '.total_count')"
 github_data+="\n<https://github.com/search?q=repo%3Aopenshift%2Fhac-dev+state%3Aopen+-label%3aapproved&type=issues | HAC PRs without APPROVED>:  $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fhac-dev+state%3Aopen+-label%3aapproved&type=issues' -H "Accept: application/json" | jq '.total_count')"
-github_data+="\n<https://github.com/search?q=repo%3Aopenshift%2Fhac-dev+repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Anemesis09+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+created%3A%3C2022-06-19+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=Issues&ref=advsearch&l=&l= | PRs Opened for more than 7 days>:  $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fhac-dev+repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Anemesis09+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+created%3A%3C2022-06-19+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=repositories' -H "Accept: application/json" | jq '.total_count')"
+github_data+="\n<https://github.com/search?q=repo%3Aopenshift%2Fhac-dev+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Anemesis09+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+created%3A%3C2022-06-19+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=Issues&ref=advsearch&l=&l= | PRs Opened for more than 7 days>:  $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fhac-dev+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Anemesis09+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+created%3A%3C2022-06-19+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=repositories' -H "Accept: application/json" | jq '.total_count')"
 
 echo "Posting on slack"
 
@@ -123,7 +123,7 @@ data='{
 	]
 }'
 
-curl -X POST -H "Content-type:application/json" --data "$data" <webhook_url>
+curl -X POST -H "Content-type:application/json" --data "$data" https://hooks.slack.com/services/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
 
 
 echo "\nDone"
