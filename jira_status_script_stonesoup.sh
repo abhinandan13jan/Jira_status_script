80a3b5ac6f7cb09fdab11a91de28c0ed4791504a
Abhi <abhinandan13jan@gmail.com>
2023-12-18 19:46:20 +0530

    add rhtap sli

--- jira_status_script_stonesoup.sh
+++ jira_status_script_stonesoup.sh
@@ -5,9 +5,9 @@ authorization="Authorization: Bearer "$1
 echo "Generating Status Report"
 
 # head="HELLO @hac-dev-team"
-head='StoneSoup' 
+head='RHTAP UI' 
 
-echo "Fetching stonesoup stories"
+echo "Fetching RHTAP stories"
 
 hac_stories='\n\n*UI Stories* :toy-story-buzz::toy-story-buzz:'
 hac_stories+="\n1. <https://issues.redhat.com/issues/?filter=12396633 | Stories Done> ($(curl -s "https://issues.redhat.com/rest/api/2/search?jql=filter=12396633" -H "Accept: application/json" | jq ".total"))"
@@ -19,22 +19,32 @@ fi
 hac_stories+="\n3. <https://issues.redhat.com/issues/?filter=12396638 | Unassigned Stories>($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396638' -H "Accept: application/json" | jq '.total'))" 
 hac_stories+="\n4. <https://issues.redhat.com/issues/?filter=12396637 | Ready for pointing stories> ($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396637' -H "Accept: application/json" | jq '.total'))" 
 
-echo "Fetching stonesoup bugs"
+echo "Fetching RHTAP bugs"
 
 hac_bugs="\n\n *Bugs:* :bug::bug:"
-hac_bugs+="\n1. <https://issues.redhat.com/issues/?filter=12396774 | In Review> ($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396774' -H "Accept: application/json" | jq '.total'))" 
-hac_bugs+="\n2. <https://issues.redhat.com/issues/?filter=12396775 | QE Review> ($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396775' -H "Accept: application/json" | jq '.total'))" 
-alert_check=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396777' -H "Accept: application/json" | jq '.total')
-hac_bugs+="\n3. <https://issues.redhat.com/issues/?filter=12396777 | Open bugs count> ($alert_check)" 
+hac_bugs+="\n1. <https://issues.redhat.com/issues/?filter=12396775 | QE Review> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396775' -H "Accept: application/json" | jq '.total'))" 
+alert_check=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12416904' -H "Accept: application/json" | jq '.total')
+hac_bugs+="\n2. <https://issues.redhat.com/issues/?filter=12416904 | Open RHTAP UI bugs> ($alert_check)" 
 if [ $alert_check -ge 45 ]; then
     hac_bugs+=" :fire::fire:"
 fi
-hac_bugs+="\n4. <https://issues.redhat.com/issues/?filter=12396778 | Triaged Bugs> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396778' -H "Accept: application/json" | jq '.total'))" 
+alert_check=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12416902' -H "Accept: application/json" | jq '.total')
+hac_bugs+="\n3. <https://issues.redhat.com/issues/?filter=12416902 | Triaged RHTAP UI bugs> ($alert_check)" 
+if [ $alert_check -ge 25 ]; then
+    hac_bugs+=" :fire::fire:"
+fi
+alert_check=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12416903' -H "Accept: application/json" | jq '.total')
+hac_bugs+="\n4. <https://issues.redhat.com/issues/?filter=12416903 | Needs info RHTAP UI bugs> ($alert_check)" 
+if [ $alert_check -ge 25 ]; then
+    hac_bugs+=" :fire::fire:"
+fi
+hac_bugs+="\n5. <https://issues.redhat.com/issues/?filter=12396778 | Triaged Bugs> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12396778' -H "Accept: application/json" | jq '.total'))" 
+hac_bugs+="\n6. <https://issues.redhat.com/issues/?filter=12426879 | Release pending bugs> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426879' -H "Accept: application/json" | jq '.total'))" 
 
 # this is block 3
-echo "Fetching stonesoup dashboard"
+echo "Fetching RHTAP dashboard"
 
-stonesoup_dashboard="\n\n *StoneSoup Dashboard:* :clipboard::clipboard:"
+stonesoup_dashboard="\n\n *RHTAP Dashboard:* :clipboard::clipboard:"
 stonesoup_dashboard+="\n1. <https://issues.redhat.com/issues/?filter=12405355 | Blocked> ($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12405355' -H "Accept: application/json" | jq '.total'))"
 alert_check=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12405357' -H "Accept: application/json" | jq '.total')
 stonesoup_dashboard+="\n2. <https://issues.redhat.com/issues/?filter=12405357 | Open stonesoup UI bugs> ($(curl -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12405357' -H "Accept: application/json" | jq '.total'))"  
@@ -47,6 +57,24 @@ stonesoup_dashboard+="\n4. <https://issues.redhat.com/issues/?filter=12405686 | 
 if [ $alert_check -ge 45 ]; then
     stonesoup_dashboard+=" :fire::fire:"
 fi
+
+echo "Fetching RHTAP bugs SLI data" 
+
+rhtap_blockers="\n\n *RHTAP Blocker bugs:*"
+rhtap_blockers+="\n1. <https://issues.redhat.com/issues/?filter=12426878 | Bloker reds :alert-siren:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426878' -H "Accept: application/json" | jq '.total'))" 
+rhtap_blockers+="\n2. <https://issues.redhat.com/issues/?filter=12426877 | Bloker yellows :large_yellow_circle: > ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426877' -H "Accept: application/json" | jq '.total'))" 
+rhtap_blockers+="\n3. <https://issues.redhat.com/issues/?filter=12426876 | Bloker greens :green-circle:: > ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426876' -H "Accept: application/json" | jq '.total'))" 
+
+rhtap_critical="\n\n *RHTAP Critical bugs:*"
+rhtap_critical+="\n1. <https://issues.redhat.com/issues/?filter=12426873 | Critical reds :alert-siren:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426873' -H "Accept: application/json" | jq '.total'))" 
+rhtap_critical+="\n2. <https://issues.redhat.com/issues/?filter=12426874 | Critical yellows :large_yellow_circle:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426874' -H "Accept: application/json" | jq '.total'))" 
+rhtap_critical+="\n3. <https://issues.redhat.com/issues/?filter=12426875 | Critical greens :green-circle:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426875' -H "Accept: application/json" | jq '.total'))" 
+
+rhtap_major="\n\n"
+rhtap_major+="\n\n *RHTAP Major bugs:*"
+rhtap_major+="\n1. <https://issues.redhat.com/issues/?filter=12426870 | Major reds :alert-siren:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426870' -H "Accept: application/json" | jq '.total'))" 
+rhtap_major+="\n2. <https://issues.redhat.com/issues/?filter=12426871 | Major yellows :large_yellow_circle:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426871' -H "Accept: application/json" | jq '.total'))" 
+rhtap_major+="\n3. <https://issues.redhat.com/issues/?filter=12426872 | Major greens :green-circle:> ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12426872' -H "Accept: application/json" | jq '.total'))" 
 
 echo "Fetching Github data"
 
@@ -92,6 +120,32 @@ data='{
 				{
 					"type": "mrkdwn",
 					"text": "'$hac_bugs'"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": "*RHTAP Bugs SLI*"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": "'$rhtap_blockers'"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "'$rhtap_critical'"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "'$rhtap_major'"
 				}
 			]
 		},
